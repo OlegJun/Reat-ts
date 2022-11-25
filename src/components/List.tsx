c70c40f5fde2f97aacd80ec3898330446ede0151
OlegJun <olegpotsah290@gmail.com>
2022-11-25 17:10:33 +0500

    finished component List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,14 @@
 import React, {FC} from 'react';
 
-const List:FC = () => {
+interface ListProps<T> {
+    items: T[];
+    renderItem: (item: T) => React.ReactNode;
+}
+
+export function List<T>(props:ListProps<T>) {
     return (
         <div>
-
+            {props.items.map(props.renderItem)}
         </div>
     );
 };
