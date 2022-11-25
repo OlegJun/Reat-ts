9ebce08743bb6eb55c7215bdea553f460f01ffd6
OlegJun <olegpotsah290@gmail.com>
2022-11-25 17:33:48 +0500

    create list todos and async loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import UserList from "./components/UserList";
-import {IUser} from "./types/types";
+import {ITodo, IUser} from "./types/types";
 import axios from "axios";
 import List from "./components/List";
 import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
 
 function App() {
     const [users, setUsers] = useState<IUser[]>([])
+    const [todos, setTodos] = useState<ITodo[]>([])
 
     useEffect(() => {
         fetchUsers()
+        fetchTodos()
     }, [])
 
     async function fetchUsers () {
@@ -21,13 +24,27 @@ function App() {
         }
     }
 
+    async function fetchTodos () {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            setTodos(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
     return (
         <div>
-            <UserList users={users}/>
             <List
                 items={users}
                 renderItem={(user: IUser) =>
                     <UserItem user={user} key={user.id}/>
+                }
+            />
+            <List
+                items={todos}
+                renderItem={(todo: ITodo) =>
+                    <TodoItem todo={todo} key={todo.id}/>
                 }
             />
         </div>
