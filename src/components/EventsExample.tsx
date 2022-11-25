1d8eeeb6b4abef1f6af51e269008dd7642605710
OlegJun <olegpotsah290@gmail.com>
2022-11-25 18:44:03 +0500

    learnin handle events and create example

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,7 +1,9 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useRef, useState} from 'react';
 
 const EventsExample: FC = () => {
     const [value, setValue] = useState<string>('')
+    const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
@@ -10,12 +12,45 @@ const EventsExample: FC = () => {
     //Обертка над событием для автокомплита
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         console.log(value)
+        console.log(inputRef.current?.value)
+    }
+
+    const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+
+    }
+
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setIsDrag(false)
+        console.log('drop')
+    }
+
+    const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setIsDrag(false)
+    }
+
+    const dragWithParentHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault()
+        setIsDrag(true)
     }
 
     return (
         <div>
-            <input value={value} onChange={changeHandler}/>
+            <input ref={inputRef} placeholder='Ungovernable'/>
+            <input value={value} onChange={changeHandler} placeholder='Managed'/>
             <button onClick={clickHandler}>fdfsdfd</button>
+            <div
+                onDrag={dragHandler}
+                draggable
+                style={{width: '200px', height: '200px', backgroundColor: 'red'}}
+            ></div>
+            <div
+                onDrag={dropHandler}
+                onDragLeave={leaveHandler}
+                onDragOver={dragWithParentHandler}
+                style={{width: '200px', height: '200px', backgroundColor: isDrag ? 'blue' : 'red', marginTop: '15px'}}
+            ></div>
         </div>
     );
 };
