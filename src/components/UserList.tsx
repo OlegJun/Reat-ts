c1b2fb35482da7fe002b458c0b698a0e09c72f38
OlegJun <olegpotsah290@gmail.com>
2022-11-25 16:15:08 +0500

    destructorization code

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import {IUser} from "../types/types";
-//import UserItem from "./UserItem";
+import UserItem from "./UserItem";
 
 interface UserListProps {
     users: IUser[];
@@ -11,9 +11,7 @@ const UserList: FC<UserListProps> = ({users}) => {
     return (
         <div>
             {users.map(user =>
-                <div style={{padding: '15px', border: '1px solid gray'}}>
-                    {user.id}. {user.name} lives in the city {user.address.city} on the street {user.address.street}
-                </div>
+                <UserItem key={user.id} user={user}/>
             )}
         </div>
     );
