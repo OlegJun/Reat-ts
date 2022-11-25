93afe0a76a413f74e96b48b0fafd60435e43f040
OlegJun <olegpotsah290@gmail.com>
2022-11-25 16:59:58 +0500

    Making an asynchronous request and populating an array of users

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import UserList from "./components/UserList";
 import {IUser} from "./types/types";
+import axios from "axios";
 
 function App() {
-    const users: IUser[] = [
-        {id: 1, name: "Leanne Graham", email: "Sincere@april.biz", address: {street: "Kulas Light", city: "Gwenborough", zipcode: "92998-3874",}},
-        {id: 2, name: "Oleg Graham", email: "Sincere@april.biz", address: {street: "Kulas Light", city: "Gwenborough", zipcode: "92998-3874",}},
-        {id: 3, name: "Sasha Graham", email: "Sincere@april.biz", address: {street: "Kulas Light", city: "Gwenborough", zipcode: "92998-3874",}}
-    ]
+    const [users, setUsers] = useState<IUser[]>([])
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    async function fetchUsers () {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
     return (
         <div>
             <UserList users={users}/>
