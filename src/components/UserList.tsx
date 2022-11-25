30d71e46d81e58c4818bddbcf2a3810be8ce2791
OlegJun <olegpotsah290@gmail.com>
2022-11-25 16:11:28 +0500

    create taskItem

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,13 +1,20 @@
 import React, {FC} from 'react';
+import {IUser} from "../types/types";
+//import UserItem from "./UserItem";
 
 interface UserListProps {
-
+    users: IUser[];
 }
 
-const UserList: FC<UserListProps> = () => {
+const UserList: FC<UserListProps> = ({users}) => {
+
     return (
         <div>
-
+            {users.map(user =>
+                <div style={{padding: '15px', border: '1px solid gray'}}>
+                    {user.id}. {user.name} lives in the city {user.address.city} on the street {user.address.street}
+                </div>
+            )}
         </div>
     );
 };
