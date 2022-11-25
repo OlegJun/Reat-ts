837066ae52eba345f8d0674129889602a5d9151a
OlegJun <olegpotsah290@gmail.com>
2022-11-25 19:23:11 +0500

    finished Todos page

--- src/page/TodosPage.tsx
+++ src/page/TodosPage.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import {ITodo} from "../types/types";
+import TodoItem from "../components/TodoItem";
+import List from "../components/List";
+import axios from "axios/index";
 
-const TodosPage = () => {
+const TodosPage:FC = () => {
+    const [todos, setTodos] = useState<ITodo[]>([])
+
+    useEffect(() => {
+        fetchTodos()
+    }, [])
+
+    async function fetchTodos () {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            setTodos(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+
     return (
-        <div>
-            
-        </div>
+        <List
+            items={todos}
+            renderItem={(todo: ITodo) =>
+                <TodoItem todo={todo} key={todo.id}/>
+            }
+        />
     );
 };
 
